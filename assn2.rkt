c7690dfaa7430e7467b6bbfb32b600855cb5ffd9
pbadola <42993461+pbadola@users.noreply.github.com>
2019-09-25 15:19:11 -0700

    Add files via upload

--- assn2.rkt
+++ assn2.rkt
@@ -85,7 +85,7 @@
     [(list 'rename (list source 'as dest) 'in body) (rename-as source
                                                    dest
                                                    (parse body))]
-    [(list 'begin exp exprs) (begin-exp (cons (parse exp) (parse exprs)))]
+    [(list 'begin exp exprs ...) (begin-exp (cons (parse exp) (map parse exprs)))]
     [(list 'with (list (? valid-identifier? id) expr) body) (with id (parse expr) (parse body))]
     [(list 'lazy-with (list (? valid-identifier? id) expr) body) (lazy-with id (parse expr) (parse body))]
     [(list 'with* a body) (parse (first (with-star-helper a body)))]        
@@ -106,7 +106,7 @@
 ;                                                       (rest a)) body)] ;; write tests 
     [else (error "")]))
 
-;; with-star-helper: 
+;; with-star-helper: (list symbol ReWAE) ReWAE -> ReWAE
 
 (define (with-star-helper a body)
   (cond [(empty? a) (list body)]
@@ -141,12 +141,13 @@
 (test (parse '{rename {k as b} in {with {k 2} k}}) (rename-as 'k 'b (with 'k (num 2) (id 'k)))) 
 
 ;; begin tests
-;(test (parse '{with {x 5} {begin {with {x 1} x} x}}) (with 'x (num 5) (begin-exp (cons (with 'x (num 1) (id 'x)) (id 'x)))))
+(test (parse '{begin {with {x 5} {with {y 1} y}} 5}) (begin-exp (list (with 'x (num 5) (with 'y (num 1) (id 'y)))
+                                                                      (num 5))))
 
-;(test (parse '{begin {+ 5 6} {{* 2 3} {- 9 1}}}) (begin-exp (cons (binop + (num 5) (num 6))
-;                                                                      (cons (binop * (num 2) (num 3))
-;                                                                            (binop - (num 9) (num 1))))))
-;
+(test (parse '{begin {+ 5 6} {* 2 3}}) (begin-exp (list (binop + (num 5) (num 6))
+                                                                  (binop * (num 2) (num 3)))))
+
+
                                                   
 
 ;; with tests
@@ -173,10 +174,10 @@
       (with 'x (num 1) (with 'y (num 2) (binop + (id 'x) (id 'y)))))
 ;
 ;
-(test (parse '{with* {{x {+ 1 2}} {y {+ x 1}}} {+ y 0}}) (with 'x (binop + (num 1) (num 2))
-                                                               (with 'y (binop + (id 'x) (num 1))
-                                                                     (binop + (id 'y) (num 0)))))
-
+;(test (parse '{with* {{x {+ 1 2}} {y {+ x 1}}} {+ y 0}}) (with 'x (binop + (num 1) (num 2))
+;                                                               (with 'y (binop + (id 'x) (num 1))
+;                                                                     (binop + (id 'y) (num 0)))))
+;
 
 
 
@@ -234,6 +235,10 @@
 (test/exn (parse '{with* {x 1} x}) "")
 (test/exn (parse 'with) "")
 (test/exn (parse '{a b c}) "")
+(test/exn (parse '+) "")
+(test/exn (parse '{+ "not" "number"}) "")
+(test/exn (parse '{begin "not" "list"}) "")
+(test/exn (parse '{begin {{+ 2 3} {+ "not" "number"}}}) "")
 
 
 
@@ -266,16 +271,27 @@
                          (subst e target-id val))]
      [with (bound-id named-expr bound-body)
           (if (symbol=? bound-id target-id)
-              ;; little confused here, when id's are = 
               (with bound-id
                     (subst named-expr target-id val)
                     bound-body)
               (with bound-id
                     (subst named-expr target-id val) 
                     (subst bound-body target-id val)))]
-    [lazy-with (bound-id named-expr bound-body) 0 ]
-    [rename-as (source-id dest-id body) 0]
-    [begin-exp (exprs) 0 ]
+    [lazy-with (bound-id named-expr bound-body)
+               (if (symbol=? bound-id target-id)
+              (lazy-with bound-id
+                    (subst named-expr target-id val)
+                    bound-body)
+              (lazy-with bound-id
+                    (subst named-expr target-id val) 
+                    (subst bound-body target-id val)))]  
+    [rename-as (source-id dest-id body) (if (symbol=? target-id source-id)
+                                            (subst body dest-id val) 
+                                            target-exp)]
+
+
+    
+    [begin-exp (exprs) (with target-id val (subst exprs target-id val))]                           ;; TODO
     [id (v) (if (symbol=? v target-id) val target-exp)]))
     
 
@@ -290,6 +306,7 @@
 (test (subst (with 'x (id 'x) (id 'x)) 'x (num 1))   
       (with 'x (num 1) (id 'x)))
 
+;(test (subst (rename-as 'y 'z (binop + (num 0) (id 'z))) 'y (num 5)) (with 'z (id 'y) (binop + (id 'z) (num 0))))
 
 ;; TODO The test provided is just an example.
 ;;      You must provide more tests for subst!
@@ -320,20 +337,33 @@
                              (interp e))]
     [with (bound-id named-expr bound-body)
           (interp (subst bound-body
-                       bound-id
-                       (num (interp named-expr))))]
+                         bound-id
+                         (num (interp named-expr))))]
     [lazy-with (bound-id named-expr bound-body)
                (interp (subst bound-body
-                       bound-id
+                              bound-id
                        ;; change this so not interpreting before substituting - how?
-                       (num (interp named-expr))))]
-    [rename-as (source dest body) (subst (interp body)
-                                         (interp source)
-                                         (interp body))] ; (subst target-exp target-id val) ;; rename x as y in (+ x 3)
-    [begin-exp (exprs) 0]
+                              named-expr))] ;before: (num (interp named-expr))
+    [rename-as (source dest body) (error "")] ; (subst target-exp target-id val) ;; rename x as y in (+ x 3)
+    [begin-exp (exprs) (cond [(empty? exprs) (error "")]
+                             [(= 1 (length (exprs))) (interp (first (exprs)))]
+                             [else
+                              (foldl (lambda (elem v)
+                                       (+ v (if (equal? (first (reverse (exprs))) elem)
+                                                (interp elem) ;; if at last of list, interp elem and keep result
+                                                (if (not (equal? (interp elem) 0))
+                                                    0
+                                                    (interp elem)))))
+                                                
+                                     0
+                                     exprs)])]
     [id (v) (error "")] 
     ))
-    
+
+
+(test (interp (lazy-with 'x (binop + (num 4) (id 'y))
+                         (num 5)))
+      5)
 
 ; TODO: write more tests for interp as needed
 (test (interp (with 'x (num 1) (id 'x))) 1)
